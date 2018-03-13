8aeff8e9468f391f9bef95d165fb85ef8012de9b
wuge <1799735174@qq.com>
2018-03-14 00:12:26 +0800

    fix_bug

--- src/test/java/DataCenterTest.java
+++ src/test/java/DataCenterTest.java
@@ -3,6 +3,7 @@ import com.hfuu.service.IDataCenterService;
 import com.hfuu.service.impl.DataCenterServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -12,10 +13,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/spring-context.xml", "classpath:spring/spring-mybatis.xml"})
 public class DataCenterTest {
+    @Autowired
+    private IDataCenterService service;
+
     @Test
     public void testDataCenter(){
-        IDataCenterService dataCenterService = new DataCenterServiceImpl();
-        dataCenterService.testLogger();
+        service.testLogger();
     }
 
     @Test
